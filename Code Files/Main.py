1af8631ccfa037a4d565b36052d1f45b259cbfe8
Mi-Yun-Shan <miyunshan@protonmail.ch>
2024-12-18 21:50:09 +0000

    Added Main file with test code for current methods

---
+++ Code Files/Main.py
@@ -0,0 +1,11 @@
+from Task import Task
+
+if __name__ == "__main__":
+    task_manager = Task()
+
+    Task.clear_tasks(task_manager)
+    # Adding tasks
+    task_manager.add_task(1, "Do laundry")
+    task_manager.add_task(2, "Write report", status="in progress")
+    task_manager.add_task(3, "Grocery shopping")
+    Task.print_tasks(task_manager)
